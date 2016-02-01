3f2596d5e540ede3c372f3161864159b3a59f4ed
Nobuo MAKIMOTO <nobuo.makimoto@gmail.com>
2016-02-02 01:50:06 +0900

    Added usage of FFT

---
+++ doc/usage_fft.C
@@ -0,0 +1,29 @@
+// Usage of FFT code
+// - Include "fft.H" and use "nmlib" namespace. No linking is required.
+// - Features include FFT, inverse FFT and their by-products.
+
+
+#include <iostream>
+#include <vector>
+#include <complex>
+#include "fft.H"
+using namespace nmlib;
+
+
+int main(void){
+  size_t n=1024, ncut=20;
+  std::vector<std::complex<double> > x(n),y, z;
+
+  for(size_t k=0; k<n; k++) x[k]=k%64;  // sample wave - triangle
+
+  y= fft(x);  // y[f] = \sum_k exp(-2\pi ikf/n) x[k]
+  z=ifft(y);  // x[k] = \sum_f exp( 2\pi ikf/n) y[f] / n
+  y=powersp(x);
+  z=lowpass(x,ncut);  // cut ncut-th frequency (ncut/T) or higher
+  //...
+
+  for(size_t k=0; k<n; k++)
+    std::cout << k << "\t" << x[k].real() << "\t" << y[k].real() << "\t" << z[k].real() << std::endl;
+
+  return 0;
+}

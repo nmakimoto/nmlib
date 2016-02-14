25be08f0ebcdf9ad755d735dcb377adfa7d2a1d8
Nobuo MAKIMOTO <nobuo.makimoto@gmail.com>
2016-02-14 22:41:51 +0900

    Added unit test code
    - Based on Google test
    - Test environment: googletest 1.6.0, g++ 4.8.4, ubuntu 14.04

---
+++ test/gtest_main.C
@@ -0,0 +1,16 @@
+// Google test for nmlib
+// Compile: g++ -o gtest_main gtest_*.C -I/usr/src/gtest -lpthread
+// Usage: $0 --help
+
+
+#include <gtest/gtest.h>
+#include <src/gtest-all.cc>
+#include <stdexcept>
+#include <iostream>
+
+
+int main(int argc, char* argv[]){
+  ::testing::InitGoogleTest(&argc,argv);
+  if(argc>1){ std::cerr<<"E unknown options <"<<argv[1]<<"...> (ABORTED)\n"; return -1; }
+  return RUN_ALL_TESTS();
+}

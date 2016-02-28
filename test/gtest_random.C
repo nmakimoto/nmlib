3f4789e1700a2cc54e9800d2740d299a2b79377d
Nobuo MAKIMOTO <nobuo.makimoto@gmail.com>
2016-02-28 22:07:00 +0900

    Added Corput class and Halton class

--- test/gtest_random.C
+++ test/gtest_random.C
@@ -39,10 +39,29 @@ TEST(random,whiteness){
 TEST(random,lowdiscrepancy){
   int n=1000000, k=10, bb[]={2,3,5,7,11};
 
+  // van der Corput sequence
   for(int i=0; i<4; i++){
     int base=bb[i];
+    Corput c(base);
     std::map<int,int> count;
-    for(int seq=0; seq<n; seq++) count[int(lds(seq,base)*k)]++;
-    for(int i=0; i<k; i++) EXPECT_NEAR(count[i], n/k, log(n)/log(base));
+    for(int seq=0; seq<n; seq++) count[int(c()*k)]++;
+    for(int j=0; j<k; j++) EXPECT_NEAR(count[j], n/k, log(n)/log(base));
+  }
+
+  // Halton sequence
+  {
+    Halton h(3);
+    std::map<int,int> cnt0,cnt1,cnt2;
+    for(int seq=0; seq<n; seq++){
+      Matrix x=h();
+      cnt0[int(x(0)*k)]++;
+      cnt1[int(x(1)*k)]++;
+      cnt2[int(x(2)*k)]++;
+    }
+    for(int j=0; j<k; j++) EXPECT_NEAR(cnt0[j], n/k, log(n)/log(2));
+    for(int j=0; j<k; j++) EXPECT_NEAR(cnt1[j], n/k, log(n)/log(3));
+    for(int j=0; j<k; j++) EXPECT_NEAR(cnt2[j], n/k, log(n)/log(5));
+
+    EXPECT_THROW(Halton(6), std::runtime_error);
   }
 }

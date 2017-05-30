1405ba1f8127c190fec1e40db2e17b9d46272fdd
Nobuo MAKIMOTO <nobuo.makimoto@gmail.com>
2017-05-31 00:05:12 +0900

    Add Levenberg-Marquardt optimization and nonlinear curve fitting

--- include/optimization.h
+++ include/optimization.h
@@ -6,6 +6,8 @@
 
 
 #include <vector>
+#include <cmath>
+#include "diff.h"
 #include "matrix.h"
 namespace nmlib {
 
@@ -17,6 +19,12 @@ namespace nmlib {
 //   iter,tol: max iteration, tolerance(final simplex size)
 template<class Func> Matrix opt_amoeba(const Func& f, const Matrix& x0, double lambda, size_t iter, double tol);
 template<class Func> Matrix opt_amoeba(const Func& f, std::vector<Matrix>& xx, std::vector<double>& ff, size_t iter, double tol);
+
+// Levenberg-Marquardt optimization - find argmin_x ||f(x)||^2  near x0
+template<class Func> Matrix opt_lma(const Func& f, const Matrix& x0, const Matrix& dx, double lambda=1.0, double nu=4.0);
+
+// Nonlinear least square curvefit using LMA - find argmin_th \sum_k |yy[k]-f(xx[k],th)|^2 near th0
+template<class Func> Matrix curvefit(const Func& f, const Matrix& xx, const Matrix& yy, const Matrix& th0, const Matrix& dth);
 
 
 /******** Implementation ********/
@@ -80,6 +88,43 @@ template<class Func> Matrix opt_amoeba(const Func& f, std::vector<Matrix>& xx, s
     }
   }
   return xx[ord[0]];
+}
+
+// Levenberg-Marquardt optimization - find argmin_x ||f(x)||^2 near x0
+template<class Func> Matrix opt_lma(const Func& f, const Matrix& x0, const Matrix& dx, double lambda, double nu){
+  Matrix x=x0, y=f(x), jf=jacobian(f,x,dx), hf=tp(jf)*jf;
+  while(true){
+    Matrix x1,y1,hf1;
+    hf1 = hf;
+    for(size_t k=0; k<hf1.nrow(); k++) hf1(k,k)*=1+lambda;
+    x1 = x - inv(hf1)*tp(jf)*y;
+    y1 = f(x1);
+    if( std::isnan(norm(x1)) || std::isinf(norm(x1)) ) break;
+    if( norm(x1-x)==0 ) break;  // converged
+    if( norm(y1)<norm(y) ){ x=x1; y=y1; jf=jacobian(f,x,dx); hf=tp(jf)*jf; if(1+lambda!=1) lambda/=nu; continue; }
+    lambda*=nu;
+  }
+  return x;
+}
+
+// Nonlinear least square curvefit using LMA - find argmin_th \sum_k |yy[k]-f(xx[k],th)|^2 near th0
+template<class Func> Matrix curvefit(const Func& f, const Matrix& xx, const Matrix& yy, const Matrix& th0, const Matrix& dth){
+  class Residual{
+    public:
+    Residual(const Func& f0, const Matrix& xx0, const Matrix& yy0): f(f0), xx(xx0), yy(yy0) {
+      if( xx.dim()!=yy.dim() ) throw std::runtime_error("Resudial: invalid data dimensions");
+    }
+    Matrix operator()(const Matrix& th) const{
+      const size_t n=yy.dim();
+      Matrix r(n);
+      for(size_t k=0; k<n; k++) r(k)=f(xx(k),th)-yy(k);
+      return r;
+    }
+    const Func& f;
+    const Matrix& xx,yy;
+  };
+  Residual r(f,xx,yy);
+  return opt_lma(r,th0,dth);
 }
 
 
--- test/gtest_optimization.cpp
+++ test/gtest_optimization.cpp
@@ -6,6 +6,7 @@
 
 #include "optimization.h"
 #include "matrix.h"
+#include "random.h"
 using namespace nmlib;
 
 
@@ -14,10 +15,61 @@ inline double f(const Matrix& x){
   return inner(x-x0,x-x0)+4.5;
 }
 
+inline Matrix g(const Matrix& x){
+  Matrix x0=Matrix({1.2,2.3,3.4});
+  Matrix y=x-x0;
+  y(0)+= y(0)*y(0)*y(0);
+  y(1) = exp(y(1))-1;
+  y(2)+= y(0)*y(1);
+  return y;
+}
+
+inline double h(double x, const Matrix& th){
+  return (x-th(0))*sqrt(fabs(th(1)));
+}
+
 
 TEST(optimization,amoeba){
   Matrix x, x0({-3,-2,-1}), x1({1.2,2.3,3.4});
   x = opt_amoeba(f,x0,0.1,500,1.e-4);
   EXPECT_NEAR( norm(x-x1), 0, 1.e-4);
   EXPECT_NEAR( f(x)-f(x1), 0, 1.e-8);
+}
+
+
+TEST(optimization,lma){
+  Matrix x, x0({-3,-2,-1}), x1({1.2,2.3,3.4}), dx({1.e-4,1.e-4,1.e-4});
+  x = opt_lma(g, x0, dx);
+  EXPECT_NEAR( norm(x-x1), 0, 1.e-4);
+  EXPECT_NEAR( norm(g(x)-g(x1)), 0, 1.e-8);
+}
+
+
+TEST(optimization,curvefit){
+  size_t n=100;
+  Matrix th({1.1,2.2}), th0({1,1}), dth({1.e-6,1.e-6});
+  Matrix xx(n), yy(n);
+
+  RNG rng(12345);
+  for(size_t k=0; k<n; k++){
+    xx(k)=rng.nrand();
+    yy(k)=h(xx(k),th) + rng.nrand()*0.1;
+  }
+
+  Matrix th1=curvefit(h,xx,yy,th0,dth);
+
+  for(size_t k=0; k<th1.dim(); k++){
+    Matrix th2=th1;
+    double err1, err2;
+
+    err1=0;  for(size_t i=0; i<xx.dim(); i++){ double dy=h(xx(i),th1)-yy(i); err1+=dy*dy; }
+
+    th2(k)=th1(k)+1.e-8;
+    err2=0;  for(size_t i=0; i<xx.dim(); i++){ double dy=h(xx(i),th2)-yy(i); err2+=dy*dy; }
+    EXPECT_LT(err1,err2);
+
+    th2(k)=th1(k)-1.e-8;
+    err2=0;  for(size_t i=0; i<xx.dim(); i++){ double dy=h(xx(i),th2)-yy(i); err2+=dy*dy; }
+    EXPECT_LT(err1,err2);
+  }
 }

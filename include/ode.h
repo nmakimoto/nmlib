b614ba94b529b5ab15613952df04af78e0c46504
Nobuo MAKIMOTO <nobuo.makimoto@gmail.com>
2017-06-27 22:14:33 +0900

    Update ODE solvers so that calculation stops exactly at T1

--- include/ode.h
+++ include/ode.h
@@ -19,7 +19,7 @@ namespace nmlib{
 //   - 4th order adaptive Runge-Kutta (Merson)
 template<class Func,class Y> std::map<double,Y> solve_ode_rk4 (const Func& f, const Y& y0, double t0, double t1, int n);
 template<class Func,class Y> std::map<double,Y> solve_ode_rk4i(const Func& f, const Y& y0, double t0, double t1, int n);
-template<class Func,class Y> std::map<double,Y> solve_ode_rk4a(const Func& f, const Y& y0, double t0, double t1, double h, double tol);
+template<class Func,class Y> std::map<double,Y> solve_ode_rk4a(const Func& f, const Y& y0, double t0, double t1, int n, double tol);
 
 
 /******** Implementation ********/
@@ -36,13 +36,13 @@ std::map<double,Y> solve_ode_rk4(const Func& f, const Y& y0, double t0, double t
   std::map<double,Y> t2y={{t0,y0},};
   double t=t0, h=(t1-t0)/n;
   Y      y=y0;
-  for(int i=0; i<n; i++){
+  while(n--){
     Y k1,k2,k3,k4;
     k1 = h*f(t,y);
     k2 = h*f(t+h/2, y+k1*0.5);
     k3 = h*f(t+h/2, y+k2*0.5);
     k4 = h*f(t+h  , y+k3    );
-    t+=h;
+    t=t1-n*h;  //t+=h;
     y+=(k1 + 2.0*k2 + 2.0*k3 + k4)/6.0;
     t2y[t]=y;
   }
@@ -64,7 +64,7 @@ std::map<double,Y> solve_ode_rk4i(const Func& f, const Y& y0, double t0, double 
   double t=t0, h=(t1-t0)/n;
   Y      y=y0;
 
-  for(int i=1; i<=n; i++){
+  while(n--){
     Y k1,k2;
     k1=h*f(t+a1*h,y);
     k2=h*f(t+a2*h,y);
@@ -82,7 +82,7 @@ std::map<double,Y> solve_ode_rk4i(const Func& f, const Y& y0, double t0, double 
     }
 
     y+=c1*k1+c2*k2;
-    t+=h;
+    t=t1-n*h;  //t+=h;
     t2y[t]=y;
   }
   return t2y;
@@ -90,12 +90,12 @@ std::map<double,Y> solve_ode_rk4i(const Func& f, const Y& y0, double t0, double 
 
 
 template<class Func,class Y>
-std::map<double,Y> solve_ode_rk4a(const Func& f, const Y& y0, double t0, double t1, double h, double tol){
+std::map<double,Y> solve_ode_rk4a(const Func& f, const Y& y0, double t0, double t1, int n, double tol){
   std::map<double,Y> t2y={{t0,y0},};
-  double t=t0;
+  double t=t0, h=(t1-t0)/n;
   Y      y=y0;
 
-  while(t<t1){
+  while(n>0){
     Y k1,k2,k3,k4,k5,dy1,dy2;
     k1 = h*f(t,y);
     k2 = h*f(t+h/3, y + k1/3.);
@@ -104,12 +104,14 @@ std::map<double,Y> solve_ode_rk4a(const Func& f, const Y& y0, double t0, double 
     k5 = h*f(t+h  , y + (k1 - 3.*k3 + 4.*k4)/2.);
     dy1=(k1        +4.*k4 +k5)/6.;
     dy2=(k1 -3.*k3 +4.*k4    )/2.;
+
     double err=ode::norm(dy1-dy2)/5;
-    if( err>tol ){ h/=2; continue; }  // Adaptive
-    t+=h;
+    if( err>tol ){ n*=2; h/=2; continue; }
+    n--;
+    t=t1-n*h;  //t+=h;
     y+=dy1;
     t2y[t]=y;
-    if( err<tol/64 ) h*=2;
+    if( err<tol/64 && n%2==0 ){ n/=2; h*=2; }
   }
   return t2y;
 }
